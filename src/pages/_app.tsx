2bda584cdc4128aa14d2f8eca00bef59368d3fe2
Luis <luis.fragment.code@gmail.com>
2023-02-08 08:26:59 -0300

    feat(page): add head properties

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,51 @@
-import { AwkiamaruTheme, Fonts, Header } from '@awkiamaru/components'
+import { AwkiamaruTheme, Fonts, Footer, Header } from '@awkiamaru/components'
+import { Box, Container } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function Website({ Component, pageProps, router }: AppProps) {
   return (
     <AwkiamaruTheme cookies={pageProps.cookies}>
       <Fonts />
-      <Header />
-      <Component {...pageProps} key={router.route} />
+      <Box as="main" pb={8}>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta
+            name="description"
+            content="Luis Fernando - Product-focused FullStack Developer"
+          />
+          <meta name="author" content="Luis Fernando" />
+          <meta name="author" content="awkiamaru" />
+          <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+          <meta
+            name="twitter:title"
+            content="Luis Fernando - Product-focused FullStack Developer"
+          />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@awkiamaru" />
+          <meta name="twitter:creator" content="@awkiamaru" />
+          <meta name="twitter:image" content="/card.png" />
+          <meta
+            property="og:site_name"
+            content="Luis Fernando - Product-focused FullStack Developer"
+          />
+          <meta
+            name="og:title"
+            content="Luis Fernando - Product-focused FullStack Developer"
+          />
+          <meta property="og:type" content="website" />
+          <meta property="og:image" content="/card.png" />
+        </Head>
+
+        <Header />
+
+        <Container maxW="container.md" pt={14}>
+          <Component {...pageProps} key={router.route} />
+          <Footer />
+        </Container>
+      </Box>
     </AwkiamaruTheme>
   )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,13 +5,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <meta charSet="utf-8" />
-        <meta
-          name="description"
-          content="Luis Fernando - Product-focused FullStack Developer"
-        />
-      </Head>
+      <Head />
       <body>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
