d9299a8cd9cded956c11e9c4e55231e0e6ece1d8
Luis <luis.fragment.code@gmail.com>
2023-02-08 08:30:12 -0300

    feat(footer): add footer content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,1 +1,13 @@
-export const Footer = () => <></>
+import { Box } from '@chakra-ui/react'
+
+export const Footer = () => (
+  <Box
+    display="flex"
+    alignItems="center"
+    justifyContent="center"
+    opacity={0.4}
+    fontSize="sm"
+  >
+    &copy; {new Date().getFullYear()} Luis Fernando Poma. All Rights Reserved.
+  </Box>
+)
